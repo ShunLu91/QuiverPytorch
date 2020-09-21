920baa0d17693de3c852ac9d2c17ec23bf3b5e26
lushun_imac <470651748@qq.com>
2020-09-21 19:44:46 +0800

    darts models

--- demo.py
+++ demo.py
@@ -2,12 +2,16 @@
 import numpy as np
 from quiver_engine import server
 from quiver_engine.model_utils import register_hook
-from torchvision import  models
+from torchvision import models
+
+from searched_models.classification.darts import genotypes
+from searched_models.classification.darts.model import NetworkCIFAR
+
 
 if __name__ == "__main__":
-    model = models.resnet18()
+    # model = models.resnet18()
+    model = NetworkCIFAR(C=16, num_classes=10, layers=20, auxiliary=False, genotype=genotypes.DARTS_V1)
 
     hook_list = register_hook(model)
     
-    server.launch(model, hook_list, input_folder="./data/Cat", image_size=[250,250], use_gpu=False)
-
+    server.launch(model, hook_list, input_folder="./data/Cat", image_size=[32, 32], use_gpu=False)
